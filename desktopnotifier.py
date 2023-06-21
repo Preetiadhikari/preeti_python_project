c40bcbdf197e95b2e12d272a670e1eb53fa02cc1
Preetiadhikari <135636414+Preetiadhikari@users.noreply.github.com>
2023-06-21 13:05:31 +0530

    Add files via upload

---
+++ desktopnotifier.py
@@ -0,0 +1,13 @@
+from plyer import notification
+import time 
+
+if __name__ == '__main__':
+      notification.notify(
+         title="***Take Rest***",
+        message="Rest is vital for better mental health,increased concentrated,reduceed stress,improved mood even a better metabolism",
+             
+            app_name="settings",
+            timeout=5 )
+      
+         
+
